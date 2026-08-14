0e502640d33a6c1447f97502874c853af3d99706
Massimo Franciosa <maxfranciosa@googlemail.com>
2026-08-14 18:50:00 +0100

    tweaks to driver market decisions

--- AMS2ChEd.Business/Services/DriverHirer.cs
+++ AMS2ChEd.Business/Services/DriverHirer.cs
@@ -31,27 +31,27 @@ namespace AMS2ChEd.Business.Services
             { TeamReputation.SUPER_MINNOW, DriverReputation.AGEING_STRONG_MIDFIELD }
         };
 
-        public static Dictionary<TeamReputation, Dictionary<DriverRole, DriverReputation[]>> teamPolicies = new()
+        private static Dictionary<TeamReputation, Dictionary<DriverRole, Tuple<DriverReputation,DriverPolicyFit>[]>> teamPolicies = new()
         {
             { 
                 TeamReputation.TOP_TEAM, new()
                 {
                     { DriverRole.FIRST_DRIVER, new[] { 
-                                                        DriverReputation.YOUNG_CHAMPIONSHIP_LEVEL,
-                                                        DriverReputation.PRIME_CHAMPIONSHIP_LEVEL,
-                                                        DriverReputation.PRIME_CHAMPIONSHIP_LEVEL_UNPROVEN,
+                                                        Tuple.Create(DriverReputation.YOUNG_CHAMPIONSHIP_LEVEL, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.PRIME_CHAMPIONSHIP_LEVEL, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.PRIME_CHAMPIONSHIP_LEVEL_UNPROVEN, DriverPolicyFit.GoodFit),
                                                      } 
                     },
                     { DriverRole.SECOND_DRIVER, new[] {
-                                                        DriverReputation.YOUNG_CHAMPIONSHIP_LEVEL,
-                                                        DriverReputation.PRIME_CHAMPIONSHIP_LEVEL,
-                                                        DriverReputation.PRIME_CHAMPIONSHIP_LEVEL_UNPROVEN,
-                                                        DriverReputation.YOUNG_CHAMPIONSHIP_LEVEL_UNPROVEN,
-                                                        DriverReputation.AGEING_CHAMPIONSHIP_LEVEL,
-                                                        DriverReputation.PRIME_CHAMPIONSHIP_LEVEL_WASHED,
-                                                        DriverReputation.AGEING_CHAMPIONSHIP_LEVEL_WASHED,
-                                                        DriverReputation.JUST_ONE_LAST_DANCE,
-                                                        DriverReputation.PRIME_STRONG_MIDFIELD
+                                                        Tuple.Create(DriverReputation.YOUNG_CHAMPIONSHIP_LEVEL, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.PRIME_CHAMPIONSHIP_LEVEL, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.PRIME_CHAMPIONSHIP_LEVEL_UNPROVEN, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.YOUNG_CHAMPIONSHIP_LEVEL_UNPROVEN, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.AGEING_CHAMPIONSHIP_LEVEL, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.PRIME_CHAMPIONSHIP_LEVEL_WASHED, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.AGEING_CHAMPIONSHIP_LEVEL_WASHED, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.JUST_ONE_LAST_DANCE, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.PRIME_STRONG_MIDFIELD, DriverPolicyFit.GoodFit)
                                                      }
                     }
                 }
@@ -60,22 +60,22 @@ namespace AMS2ChEd.Business.Services
                 TeamReputation.MIDFIELD_HIGH, new()
                 {
                     { DriverRole.FIRST_DRIVER, new[] {
-                                                        DriverReputation.PRIME_CHAMPIONSHIP_LEVEL_WASHED,
-                                                        DriverReputation.PRIME_CHAMPIONSHIP_LEVEL_UNPROVEN,
-                                                        DriverReputation.YOUNG_CHAMPIONSHIP_LEVEL_UNPROVEN,
-                                                        DriverReputation.PRIME_STRONG_MIDFIELD,
-                                                        DriverReputation.JUST_ONE_LAST_DANCE,
-                                                        DriverReputation.AGEING_STRONG_MIDFIELD
+                                                        Tuple.Create(DriverReputation.PRIME_CHAMPIONSHIP_LEVEL_WASHED, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.PRIME_CHAMPIONSHIP_LEVEL_UNPROVEN, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.YOUNG_CHAMPIONSHIP_LEVEL_UNPROVEN, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.PRIME_STRONG_MIDFIELD, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.JUST_ONE_LAST_DANCE, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.AGEING_STRONG_MIDFIELD, DriverPolicyFit.GoodFit)
                                                      }
                     },
                     { DriverRole.SECOND_DRIVER, new[] {
-                                                        DriverReputation.PRIME_CHAMPIONSHIP_LEVEL_WASHED,
-                                                        DriverReputation.AGEING_CHAMPIONSHIP_LEVEL_WASHED,
-                                                        DriverReputation.YOUNG_CHAMPIONSHIP_LEVEL_UNPROVEN,
-                                                        DriverReputation.PRIME_STRONG_MIDFIELD,
-                                                        DriverReputation.JUST_ONE_LAST_DANCE,
-                                                        DriverReputation.AGEING_STRONG_MIDFIELD,
-                                                        DriverReputation.PRIME_MIDFIELD
+                                                        Tuple.Create(DriverReputation.PRIME_CHAMPIONSHIP_LEVEL_WASHED, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.AGEING_CHAMPIONSHIP_LEVEL_WASHED, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.YOUNG_CHAMPIONSHIP_LEVEL_UNPROVEN, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.PRIME_STRONG_MIDFIELD, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.JUST_ONE_LAST_DANCE, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.AGEING_STRONG_MIDFIELD, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.PRIME_MIDFIELD, DriverPolicyFit.GoodFit)
                                                      }
                     }
                 }
@@ -84,18 +84,18 @@ namespace AMS2ChEd.Business.Services
                 TeamReputation.MIDFIELD, new()
                 {
                     { DriverRole.FIRST_DRIVER, new[] {
-                                                        DriverReputation.PRIME_STRONG_MIDFIELD,
-                                                        DriverReputation.JUST_ONE_LAST_DANCE,
-                                                        DriverReputation.AGEING_STRONG_MIDFIELD,
-                                                        DriverReputation.PRIME_MIDFIELD,
-                                                        DriverReputation.AGEING_MIDFIELD,
+                                                        Tuple.Create(DriverReputation.PRIME_STRONG_MIDFIELD, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.JUST_ONE_LAST_DANCE, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.AGEING_STRONG_MIDFIELD, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.PRIME_MIDFIELD, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.AGEING_MIDFIELD, DriverPolicyFit.GoodFit),
                                                      }
                     },
                     { DriverRole.SECOND_DRIVER, new[] {
-                                                        DriverReputation.AGEING_STRONG_MIDFIELD,
-                                                        DriverReputation.PRIME_MIDFIELD,
-                                                        DriverReputation.YOUNG_TALENT,
-                                                        DriverReputation.AGEING_MIDFIELD,
+                                                        Tuple.Create(DriverReputation.AGEING_STRONG_MIDFIELD, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.PRIME_MIDFIELD, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.YOUNG_TALENT, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.AGEING_MIDFIELD, DriverPolicyFit.GoodFit),
                                                      }
                     }
                 }
@@ -104,15 +104,15 @@ namespace AMS2ChEd.Business.Services
                 TeamReputation.MINNOW, new()
                 {
                     { DriverRole.FIRST_DRIVER, new[] {
-                                                        DriverReputation.PRIME_MIDFIELD,
-                                                        DriverReputation.AGEING_MIDFIELD,
-                                                        DriverReputation.YOUNG_TALENT,
-                                                        DriverReputation.PAY_DRIVER_SEASON,
+                                                        Tuple.Create(DriverReputation.PRIME_MIDFIELD, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.AGEING_MIDFIELD, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.YOUNG_TALENT, DriverPolicyFit.GoodFit),
+                                                        Tuple.Create(DriverReputation.PAY_DRIVER_SEASON, DriverPolicyFit.GoodFit),
                                                      }
                     },
                     { DriverRole.SECOND_DRIVER, new[] {
-                                                        DriverReputation.YOUNG_TALENT,
-                                                        DriverReputation.PAY_DRIVER_SEASON,
+                                                        Tuple.Create(DriverReputation.YOUNG_TALENT, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.PAY_DRIVER_SEASON, DriverPolicyFit.PerfectFit),
                                                      }
                     }
                 }
@@ -121,22 +121,38 @@ namespace AMS2ChEd.Business.Services
                 TeamReputation.SUPER_MINNOW, new()
                 {
                     { DriverRole.FIRST_DRIVER, new[] {
-                                                        DriverReputation.YOUNG_TALENT,
-                                                        DriverReputation.PAY_DRIVER_SEASON,
+                                                        Tuple.Create(DriverReputation.YOUNG_TALENT, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.PAY_DRIVER_SEASON, DriverPolicyFit.PerfectFit),
                                                      }
                     },
                     { DriverRole.SECOND_DRIVER, new[] {
-                                                        DriverReputation.YOUNG_TALENT,
-                                                        DriverReputation.PAY_DRIVER_SEASON,
+                                                        Tuple.Create(DriverReputation.YOUNG_TALENT, DriverPolicyFit.PerfectFit),
+                                                        Tuple.Create(DriverReputation.PAY_DRIVER_SEASON, DriverPolicyFit.PerfectFit),
                                                      }
                     }
                 }
             },
         };
+
+        public enum DriverPolicyFit
+        {
+            UnderQualified,
+            GoodFit,
+            PerfectFit,
+            OverQualified
+        }
+
+        public DriverPolicyFit DoesDriverFitTeamPolicy(DriverReputation driverReputation, DriverRole role, TeamReputation teamReputation)
+        {
+            if (!teamPolicies[teamReputation][role].Select(p => p.Item1).Contains(driverReputation))
+                return driverReputation > teamPolicies[teamReputation][role].Select(p => p.Item1).Max() ? DriverPolicyFit.OverQualified : DriverPolicyFit.UnderQualified;
+            return teamPolicies[teamReputation][role].FirstOrDefault(p => p.Item1 == driverReputation)?.Item2 ?? DriverPolicyFit.GoodFit;
+        }
+
         public DriverResume? PickBestCandidate(IEnumerable<DriverResume> drivers, DriverRole role, TeamReputation teamReputation)
         {
             var result = drivers?
-                    .OrderByDescending(d => teamPolicies[teamReputation][role].Contains(d.Reputation))
+                    .OrderByDescending(d => teamPolicies[teamReputation][role].Select(p => p.Item1).Contains(d.Reputation))
                     .ThenByDescending(d => d.Reputation)
                     .FirstOrDefault();
 
--- AMS2ChEd.Business/Services/OffSeasonMovements.cs
+++ AMS2ChEd.Business/Services/OffSeasonMovements.cs
@@ -173,9 +173,25 @@ namespace AMS2ChEd.Business.Services
                     continue;
                 }
 
+                var weightFit = (DriverReputation driverReputation) =>
+                {
+                    var fit = _driverHirer.DoesDriverFitTeamPolicy(driverReputation, ballot.OriginalTeamHiring.Role, ballot.OriginalTeamHiring.TeamReputation);
+
+                    switch (fit)
+                    {
+                        case DriverHirer.DriverPolicyFit.OverQualified:
+                        case DriverHirer.DriverPolicyFit.PerfectFit:
+                            return 2;
+                        case DriverHirer.DriverPolicyFit.GoodFit:
+                            return 1;
+                        default:
+                            return 0;
+                    }
+                };
+
                 var bestCandidateResume = availableCandidates
                                             .GroupBy(x => x.DriverReputation)
-                                            .OrderByDescending(g => g.Key)
+                                            .OrderByDescending(g => weightFit(g.Key))
                                             .SelectMany(g => g.OrderBy(x => rnd.Next()))
                                             .Select(g => new DriverResume { Id = g.DriverId, Reputation = g.DriverReputation })
                                             .OrderBy(d => ballot.OriginalTeamHiring.ExcludeDriverIds?.Contains(d.Id))
